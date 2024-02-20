48fd760e19f26a0c3fdca1597a5abe2509405d09
dice-2004 <135799601+dice-2004@users.noreply.github.com>
2024-02-20 16:15:21 +0900

    logアプリケーション

--- write_log.py
+++ write_log.py
@@ -5,5 +5,5 @@ TXT_LOG = 'history.log'
 def WriteLog(name,type,Message):#LOG書き込み関数
     now = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
     f = open(TXT_LOG, 'a')
-    f.write(f'{now} || {type.ljust(10)}| {Message.ljust(30)}| {name}\n')
+    f.write(f'{now} || {type.ljust(10)}| {Message.ljust(40)}| {name}\n')
     f.close()
