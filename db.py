6a53629a25c5ec5106ec40a9e0df425a3c330d36
dice-2004 <135799601+dice-2004@users.noreply.github.com>
2024-02-20 16:15:01 +0900

    DBアプリケーション

--- db.py
+++ db.py
@@ -4,7 +4,5 @@ DATABASE = "DB.db"
 
 def create_event_data():
     con = sqlite3.connect(DATABASE)
-    con.execute("CREATE TABLE IF NOT EXISTS event_data (event TEXT, day TEXT)")
+    con.execute("CREATE TABLE IF NOT EXISTS event_data (ID INTEGER PRIMARY KEY AUTOINCREMENT, event TEXT, day TEXT)")
     con.close()
-
-
