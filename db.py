c68a5283f7fc9c93ee2d29a5feed639b3cddd0ea
dice-2004 <135799601+dice-2004@users.noreply.github.com>
2024-02-21 17:04:40 +0900

    削除

--- db.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-DATABASE = "DB.db"
-
-def create_event_data():
-    con = sqlite3.connect(DATABASE)
-    con.execute("CREATE TABLE IF NOT EXISTS event_data (ID INTEGER PRIMARY KEY AUTOINCREMENT, event TEXT, day TEXT)")
-    con.close()
