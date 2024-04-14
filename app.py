0fed7260ce675c7feafd62946df721b49857b5ac
dice-2004 <135799601+dice-2004@users.noreply.github.com>
2024-04-14 23:14:09 +0900

    変更

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for,session
 import sqlite3
 import write_log
 import os
@@ -7,25 +7,60 @@ import func
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.urandom(24)
 
-
 DATABASE = "DB.db"
 TXT_LOG = "history.log"
 DBTABLE = "event_data"
-PASSKEY = "Aaqaqaq20!"
+
+def login_required(func):
+    import functools
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if session.get("username") is None:
+            return redirect(url_for("login"))
+        else:
+            return func(*args, **kwargs)
+    return wrapper
 
 
 func.create_event_data()
-
+func.create_user_data()
+#func.add_user("dice","Aaqaqaq20!")
 
 @app.route("/")
 def home():
     return redirect(url_for("top"))
 
-
 @app.route("/top")
 def top():
     write_log.WriteLog("/top", "GET", "connect web site.")
-    return render_template("top.html")
+    return render_template("top.html",key="login")
+
+
+@app.route("/products")
+def products():
+    return render_template("products.html",key="login")
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        PASS = func.receive_userdata(username)
+        print(PASS)
+        if PASS == password:
+            session["username"] = username
+            return redirect(url_for("administrator"))
+        else:
+            return render_template("login.html",error=2,key="login")
+    else:
+        return render_template("login.html",key="login")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+
+
 
 
 @app.route("/to_do")
@@ -33,37 +68,36 @@ def to_do():
     data = []
     data = func.receiveAllData_to_do()
     write_log.WriteLog("/to_do", "GET", "connect web site.")
-    return render_template("/to_do.html", datas=data)
+    return render_template("/to_do.html", datas=data,key="login")
 
 
 @app.route("/administrator_to_do")
+@login_required
 def administrator_to_do():
     data = []
     data = func.receiveAllData_to_do()
     write_log.WriteLog("/administrator_to_do", "GET", "connect web site.")
-    return render_template("administrator_to_do.html", datas=data)
+    return render_template("administrator_to_do.html", datas=data,key="logout")
 
 
 @app.route("/form_to_do", methods=["GET", "POST"])
+@login_required
 def form_to_do():
     if request.method == "POST":
-        passkey = request.form["password"]
-        if passkey == PASSKEY:
-            event = request.form["event"]
-            time = request.form["time"]
-            time = func.conversionTime(time)
-            func.writeData_to_do(event,time)
-            write_log.WriteLog("/form_to_do", "POST", "add data success!")
-            return redirect(url_for("administrator"))
-        else:
-            write_log.WriteLog("/form_to_do", "POST", "passkey error.")
-            return render_template("form_to_do.html", passs=0)
+        event = request.form["event"]
+        time = request.form["time"]
+        time = func.conversionTime(time)
+        func.writeData_to_do(event,time)
+        write_log.WriteLog("/form_to_do", "POST", "add data success!")
+        return redirect(url_for("administrator"))
+
     else:
         write_log.WriteLog("/form_to_do", "GET", "connect web site.")
-        return render_template("form_to_do.html", passs=1)
+        return render_template("form_to_do.html", passs=1,key="logout")
 
 
 @app.route("/<int:ID>/delete_to_do")
+@login_required
 def deltete_to_do(ID):
     func.deleteData_to_do(ID)
     write_log.WriteLog(
@@ -73,30 +107,28 @@ def deltete_to_do(ID):
 
 
 @app.route("/<int:ID>/update_to_do", methods=["GET", "POST"])
+@login_required
 def update_to_do(ID):
     if request.method == "POST":
-        passkey = request.form["password"]
-        if passkey == "Aaqaqaq20!":
-            event = request.form["event"]
-            time = request.form["time"]
-            time = func.conversionTime(time)
-            func.update_to_do(event,time,ID)
-            write_log.WriteLog("/" + str(ID) + "/update_to_do","POST","update data success! ID = " + str(ID),)
-            return redirect(url_for("administrator_to_do"))
-        else:
-            data=func.receiveData_to_do_ID(ID)
-            write_log.WriteLog("/" + str(ID) + "/update_to_do","POST","passkey error. ID = " + str(ID),)
-            return render_template("update_to_do.html", passs=0, datas=data)
+
+        event = request.form["event"]
+        time = request.form["time"]
+        time = func.conversionTime(time)
+        func.update_to_do(event,time,ID)
+        write_log.WriteLog("/" + str(ID) + "/update_to_do","POST","update data success! ID = " + str(ID),)
+        return redirect(url_for("administrator_to_do"))
+
     else:
         data=func.receiveData_to_do_ID(ID)
         write_log.WriteLog("/" + str(ID) + "/update_to_do", "GET", "connect web site. ID = " + str(ID))
-        return render_template("update_to_do.html", passs=1, datas=data)
+        return render_template("update_to_do.html", passs=1, datas=data,key="logout")
 
 
 @app.route("/administrator")  # 管理者用ページ
+@login_required
 def administrator():
     write_log.WriteLog("/administrator", "GET", "connect web site.")
-    return render_template("administrator.html")
+    return render_template("administrator.html",key="logout")
 
 
 if __name__ == "__main__":  # Flask起動
--- func.py
+++ func.py
@@ -1,4 +1,5 @@
 import sqlite3
+from flask import session,redirect,url_for
 
 DATABASE = "DB.db"
 
@@ -17,6 +18,16 @@ def create_event_data():
     )
     con.close()
 
+def create_user_data():
+    con = sqlite3.connect(DATABASE)
+    data=con.execute(
+        "CREATE TABLE IF NOT EXISTS user_data (ID INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE, password TEXT)"
+    ).fetchall()
+    con.close()
+
+    return data
+
+
 
 def receiveAllData_to_do():
     con = sqlite3.connect(DATABASE)
@@ -25,6 +36,16 @@ def receiveAllData_to_do():
     data = [{"ID": row[0], "event": row[1], "time": row[2]} for row in db_data]
     return data
 
+def receive_userdata(username):
+    con = sqlite3.connect(DATABASE)
+    cur = con.cursor()
+    cur.execute("SELECT * FROM user_data WHERE username = ?", (username,))
+    db_data = cur.fetchone()
+    con.close()
+    if db_data:
+        return db_data[2]  # パスワードを返す
+    else:
+        return None  # ユーザーデータが見つからない場合はNoneを返す
 
 def writeData_to_do(event,time):
     con = sqlite3.connect(DATABASE)
@@ -53,3 +74,20 @@ def receiveData_to_do_ID(ID):
     con.close()
     data = {"ID": db_data[0][0], "event": db_data[0][1], "time": db_data[0][2]}
     return data
+
+
+def add_user(username, password):
+    con = sqlite3.connect(DATABASE)
+    cur = con.cursor()
+
+    # ユーザー名がすでに存在するかどうかをチェック
+    cur.execute("SELECT * FROM user_data WHERE username = ?", (username,))
+    existing_user = cur.fetchone()
+    if existing_user:
+        print("ユーザー名はすでに存在します。別のユーザー名を選択してください。")
+        return
+
+    # 新しいユーザーを挿入
+    cur.execute("INSERT INTO user_data (username, password) VALUES (?, ?)", (username, password))
+    con.commit()
+    con.close()
